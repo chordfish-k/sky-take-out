940cde58eaba75c5a4555fb62ec6198689ce62bc
chordfish <592229466@qq.com>
2023-10-06 19:40:34 +0800

    fix:调整用户端菜品缓存相关代码

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,13 +36,14 @@ public class DishController {
      */
     @PostMapping
     @ApiOperation("新增菜品")
+    @CacheEvict(cacheNames = "setmealCache", key = "#dishDTO.categoryId")
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
 
         // 清理缓存数据
-        String key = "dish_" + dishDTO.getCategoryId();
-        cleanCache(key);
+//        String key = "dish_" + dishDTO.getCategoryId();
+//        cleanCache(key);
 
         return Result.success();
     }
@@ -66,12 +68,13 @@ public class DishController {
      */
     @DeleteMapping
     @ApiOperation("菜品批量删除")
+    @CacheEvict(cacheNames = "dishCache", allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("菜品批量删除：{}", ids);
         dishService.deleteBatch(ids);
 
         // 清理所有dish_缓存数据
-        cleanCache("dish_*");
+//        cleanCache("dish_*");
         return  Result.success();
     }
 
@@ -95,24 +98,26 @@ public class DishController {
      */
     @PutMapping
     @ApiOperation("修改菜品")
+    @CacheEvict(cacheNames = "dishCache", allEntries = true)
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
 
         // 清理所有dish_缓存数据
-        cleanCache("dish_*");
+//        cleanCache("dish_*");
 
         return Result.success();
     }
 
     @PostMapping("/status/{status}")
     @ApiOperation("起售或停售菜品")
+    @CacheEvict(cacheNames = "dishCache", allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("起售或停售菜品：{}，{}", status, id);
         dishService.startOrStop(status, id);
 
         // 清理所有dish_缓存数据
-        cleanCache("dish_*");
+//        cleanCache("dish_*");
 
         return Result.success();
     }
